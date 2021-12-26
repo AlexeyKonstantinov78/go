92bc4d4e3eaf6641acbca07d599a7a9415852420
AlexeyKonstantinov78 <74707207+AlexeyKonstantinov78@users.noreply.github.com>
2021-12-26 12:23:17 +0300

    update time

--- css/form/datepicker/datepicker.js
+++ css/form/datepicker/datepicker.js
@@ -3,19 +3,13 @@ function datepicker() {
 
     $( ".book__input_data" ).datepicker({
         dateFormat: "yy-mm-dd",
+        // dateFormat: "dd.mm.yy",
         //beforeShowDay: $.datepicker.noWeekends, // невыбираем выходные
         minDate: 'null', // минимальная дата
         autoSize: true,          
         numberOfMonths: 1,
         showButtonPanel: true,   
-    });
-    
-    console.log($('.book__input_data').val());
-    if ($('.book__input_data').val() == '') {
-
-        console.log($('div[pseudo="-webkit-datetime-edit-fields-wrapper"]'));
-        $('div[pseudo="-webkit-datetime-edit-fields-wrapper"]').html("<span>Дата</span>");
-    };
+    });       
 } 
 
 export default datepicker;
--- css/form/time/time.js
+++ css/form/time/time.js
@@ -1,12 +1,19 @@
-import { TimepickerUI } from './timepicker-ui.esm.js';
+import './time-picker/jquery.timepicker.js';
 
 function timejs() {
-    // const timepicker = document.querySelector('.version-24h');
-    // const options = {
-    //     clockType: '24h',               
-    // };
-    // const newTimepicker = new TimepickerUI(timepicker, options);
-    // newTimepicker.create();
+    $(document).ready(function(){
+        $('.book__input_time').timepicker({
+            timeFormat: 'HH:mm',
+            interval: 60,
+            minTime: '10',
+            maxTime: '7:00 pm',
+            defaultTime: 'now',
+            startTime: '10:00',
+            dynamic: false,
+            dropdown: true,
+            scrollbar: true
+        });    
+    });
 }
 
 export default timejs;
